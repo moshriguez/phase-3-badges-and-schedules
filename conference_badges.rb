26559da67d3635e5c6804eb358719cce14ff7b0e
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-17 09:40:33 -0700

    commit tree 2e6c916456096872688160824b156ac03d3122ab

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list)
+    badge_list = []
+    list.each do |name|
+        badge_list.push(badge_maker name)
+    end
+    badge_list
+end
+
+def assign_rooms(list)
+    room_assignments = []
+    list.each.with_index(1) do |name, index|
+        room_assignments.push("Hello, #{name}! You'll be assigned to room #{index}!")
+    end
+    room_assignments
+end
+
+def printer(list)
+    batch_badge_creator(list).each do |item|
+        puts item
+    end
+    assign_rooms(list).each do |next_item|
+        puts next_item
+    end
+end
